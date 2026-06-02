b9c3704a1d6c816ae52804d0dabf8957bb7563d6
Emanuel Rabina <emanuelrabina@gmail.com>
2026-06-02 22:49:20 +1200

    Scaffolding for an empty scene w/ debug elements

---
+++ source/nz/net/ultraq/breakout/BreakoutScene.groovy
@@ -0,0 +1,41 @@
+/*
+ * Copyright 2026, Emanuel Rabina (http://www.ultraq.net.nz/)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package nz.net.ultraq.breakout
+
+import nz.net.ultraq.redhorizon.graphics.Camera
+import nz.net.ultraq.redhorizon.scenegraph.Scene
+import static nz.net.ultraq.breakout.ScopedValues.WINDOW
+
+/**
+ * Scene setup for the Breakout game.
+ *
+ * @author Emanuel Rabina
+ */
+class BreakoutScene extends Scene {
+
+	final Camera camera
+
+	/**
+	 * Constructor, create a new scene.
+	 */
+	BreakoutScene() {
+
+		var window = WINDOW.get()
+
+		camera = addAndReturnChild(new Camera(1280, 800, window))
+	}
+}
